95162d21dfb63519d94a17adaaeb7f579987f02a
Jurjj <agri1987cola@gmail.com>
2023-08-05 16:51:54 +0200

    mutlityped rv created

--- app/src/main/java/com/example/myapplication/CustomAdapter.java
+++ app/src/main/java/com/example/myapplication/CustomAdapter.java
@@ -4,10 +4,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
-
 import androidx.recyclerview.widget.RecyclerView;
 
-public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.ViewHolder> {
+public class CustomAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
 
     private String[] localDataSet;
     private String[] localDataSet2;
@@ -16,26 +15,40 @@ public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.ViewHolder
      * Provide a reference to the type of views that you are using
      * (custom ViewHolder)
      */
-    public static class ViewHolder extends RecyclerView.ViewHolder {
+    public static class ViewHolder1 extends RecyclerView.ViewHolder {
         private final TextView textView;
         private final TextView textView2;
 
-        public ViewHolder(View view) {
+        public ViewHolder1(View view) {
             super(view);
             // Define click listener for the ViewHolder's View
-
             textView = (TextView) view.findViewById(R.id.textView);
             textView2 = (TextView) view.findViewById(R.id.textView2);
-
         }
 
         public TextView getTextView() {
             return textView;
         }
-      public TextView getTextView2() {
+        public TextView getTextView2() {
             return textView2;
-      }
+        }
 
+    }
+
+    /**
+     * Provide a reference to the type of views that you are using
+     * (custom ViewHolder)
+     */
+    public static class ViewHolder2 extends RecyclerView.ViewHolder {
+        private final TextView textView;
+        public ViewHolder2(View view) {
+            super(view);
+            // Define click listener for the ViewHolder's View
+            textView = (TextView) view.findViewById(R.id.textView);
+        }
+        public TextView getTextView() {
+            return textView;
+        }
     }
 
     /**
@@ -48,26 +61,45 @@ public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.ViewHolder
         localDataSet = dataSet;
         localDataSet2 = dataSet2;
     }
-
     // Create new views (invoked by the layout manager)
     @Override
-    public ViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
         // Create a new view, which defines the UI of the list item
-        View view = LayoutInflater.from(viewGroup.getContext())
-                .inflate(R.layout.text_row_item, viewGroup, false);
-
-        return new ViewHolder(view);
+        switch (viewType) {
+            case 0:
+                View view = LayoutInflater.from(viewGroup.getContext())
+                        .inflate(R.layout.text_row_item, viewGroup, false);
+                return new ViewHolder1(view);
+            case 2:
+                view = LayoutInflater.from(viewGroup.getContext())
+                        .inflate(R.layout.text_row_item_2, viewGroup, false);
+                return new ViewHolder2(view);
+            default:
+                return null;
+        }
     }
-
+    @Override
+    public int getItemViewType(int position) {
+        // Just as an example, return 0 or 2 depending on position
+        // Note that unlike in ListView adapters, types don't have to be contiguous
+        return position % 2 * 2;
+    }
     // Replace the contents of a view (invoked by the layout manager)
     @Override
-    public void onBindViewHolder(ViewHolder viewHolder, final int position) {
-
+    public void onBindViewHolder(RecyclerView.ViewHolder viewHolder, final int position) {
         // Get element from your dataset at this position and replace the
         // contents of the view with that element
-        viewHolder.getTextView().setText(localDataSet[position]);
-        viewHolder.getTextView2().setText(localDataSet2[position]);
-
+        switch (viewHolder.getItemViewType()) {
+            case 0:
+                ViewHolder1 viewHolder1 = (ViewHolder1)viewHolder;
+                viewHolder1.getTextView().setText(localDataSet[position]);
+                viewHolder1.getTextView2().setText(localDataSet2[position]);
+                break;
+            case 2:
+                ViewHolder2 viewHolder2 = (ViewHolder2) viewHolder;
+                viewHolder2.getTextView().setText(localDataSet[position]);
+                break;
+        }
     }
 
     // Return the size of your dataset (invoked by the layout manager)
